a5e03b977fe17332101afda00eb47e4f97ecc8d1
alpha3002025 <alpha300uk@gmail.com>
2025-10-17 17:48:15 +0900

    feat: 카프카/레디스 다운시 이벤트 영속화 방안 적용 + 댓글깊이 제한 - PVC 전용 모듈 구현 - kafka failure 이벤트 PVC 기록 서비스 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,16 @@
-plugins {
-	java
-	id("org.springframework.boot") version "3.5.6"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-group = "click.dailyfeed"
-version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
 dependencies {
+	/// web
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	/// jackson
+	implementation("com.fasterxml.jackson.core:jackson-databind")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
+	/// lombok
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
+	/// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
