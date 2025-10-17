8fa9bd1bae87ad90bab00d4b1d7dd7c0f85dad52
alpha3002025 <alpha300uk@gmail.com>
2025-10-17 15:08:28 +0900

    initial status

---
+++ src/main/java/click/dailyfeed/pvc/PvcApplication.java
@@ -0,0 +1,13 @@
+package click.dailyfeed.pvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvcApplication.class, args);
+	}
+
+}
