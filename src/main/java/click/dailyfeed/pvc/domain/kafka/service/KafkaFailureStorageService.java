f20b077e4623c0189e606c375e74d6136f4c2062
alpha3002025 <alpha300uk@gmail.com>
2025-10-19 20:02:10 +0900

    feat: 카프카 Publisher,Listener 의 통신실패 처리기능 개발

--- src/main/java/click/dailyfeed/pvc/domain/kafka/service/KafkaFailureStorageService.java
+++ src/main/java/click/dailyfeed/pvc/domain/kafka/service/KafkaFailureStorageService.java
@@ -2,9 +2,7 @@ package click.dailyfeed.pvc.domain.kafka.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
